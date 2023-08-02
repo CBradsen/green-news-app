f716b3c54f925cac7557fd5154849703dd8d4019
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-08-02 13:55:45 -0600

    Setup: Add logo to page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import genlogo from './genlogo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={genlogo} className="App-logo" alt="green-energy-news-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/MockData/MockData.js
@@ -0,0 +1,84 @@
+export const MockData ={
+status: "ok",
+totalResults: 33,
+articles: [
+{
+source: {
+  id: "bbc-news",
+  name: "BBC News"
+  },
+author: "https://www.facebook.com/bbcnews",
+title: "John Kerry in Beijing: Can US and China set aside rivalry for climate action?",
+description: "The two biggest economies and polluters need to work together to avert the crisis, experts say.",
+url: "https://www.bbc.co.uk/news/world-asia-china-66185535",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/B01E/production/_130368054_gettyimages-1258958730.jpg",
+publishedAt: "2023-07-15T22:12:59Z",
+content: "As John Kerry touches down in China, the main question will be whether the world's biggest superpowers - and polluters - can dispel diplomatic tensions to focus on key climate goals. Mr Kerry, the U… [+6179 chars]"
+},
+{
+source: {
+id: "bbc-news",
+name: "BBC News"
+},
+author: "https://www.facebook.com/bbcnews",
+title: "Renewables, nuclear and fossil fuels: The UK's changing energy mix",
+description: "To meet climate change targets, more of the UK's energy needs to come from low-carbon sources.",
+url: "https://www.bbc.co.uk/news/business-63976805",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/16707/production/_128211919_gettyimages-157230551.jpg",
+publishedAt: "2023-07-31T09:44:36Z",
+content: "To help tackle climate change, the UK has pledged to reach 'net zero' - where no further planet-warming greenhouse gases are added to the atmosphere - by 2050. To achieve this, much less energy nee… [+5421 chars]"
+},
+{
+source: {
+id: "bbc-news",
+name: "BBC News"
+},
+author: "https://www.facebook.com/bbcnews",
+title: "Facts are up against fear ahead of Fukushima water release",
+description: "Locals struggle to believe the plant's treated radioactive water is safe to pump into the Pacific Ocean.",
+url: "https://www.bbc.co.uk/news/world-asia-66173431",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/EE94/production/_130367016_gettyimages-514745608.jpg",
+publishedAt: "2023-07-13T21:54:57Z",
+content: "Media caption, Watch: Shaimaa Khalil visits the treatment plant to see how it works In a white coat and gloves, Ai Kimura is cutting up a fish sample at the Tarachine lab, about an hour's drive from… [+9277 chars]"
+},
+{
+source: {
+id: "bbc-news",
+name: "BBC News"
+},
+author: "https://www.facebook.com/bbcnews",
+title: "Young climate activists turn up the heat on Keir Starmer",
+description: "The group that disrupted the Labour leader's speech says the party is backsliding on energy pledges.",
+url: "https://www.bbc.co.uk/news/uk-politics-66201614",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/EB44/production/_130382206_keirchallengeconference2020.jpg",
+publishedAt: "2023-07-16T10:21:30Z",
+content: "Going into the next general election, Labour's plan to pour billions of pounds into clean energy will be one of the clear dividing lines between it and the Conservatives. A pledge to invest £28bn a … [+6618 chars]"
+},
+{
+source: {
+id: "bbc-news",
+name: "BBC News"
+},
+author: "https://www.facebook.com/bbcnews",
+title: "What does the scrapping of a wind farm plan mean for UK renewable energy?",
+description: "Vattenfall stops work on the Norfolk Boreas offshore wind farm over soaring costs.",
+url: "https://www.bbc.co.uk/news/uk-england-norfolk-66267860",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/852E/production/_122049043_norfolkboreasvattenfall.jpg",
+publishedAt: "2023-07-21T21:33:34Z",
+content: "Work has stopped on one of the UK's largest offshore wind farms after its developer said it no longer made financial sense to continue. The government has a target of doubling wind capacity by 2030… [+6727 chars]"
+},
+{
+source: {
+id: "bbc-news",
+name: "BBC News"
+},
+author: "https://www.facebook.com/bbcnews",
+title: "Norfolk Boreas: Work on offshore wind farm stops over soaring costs",
+description: "Vattenfall cites deteriorating market conditions in its decision to shelve the offshore wind farm.",
+url: "https://www.bbc.co.uk/news/uk-england-norfolk-66263340",
+urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/110C/production/_130446340_2f9a9ebd-7493-4b77-b66e-3e2c54dfa40e.jpg",
+publishedAt: "2023-07-20T19:12:32Z",
+content: "Work has stopped on one of the UK's largest offshore wind farms after its developer said it no longer made financial sense to continue. The Swedish energy giant Vattenfall is to shut down developmen… [+3446 chars]"
+}
+]
+}
