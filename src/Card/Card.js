51995514cff02dab9bfaf3516ac5ae10c531088d
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-08-02 19:23:27 -0600

    Feature: Increase height of cards to show more of the description and type tag

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,14 +4,12 @@ import '../Card/Card.css';
 
 function Card({ article }) {
   return (
-    <div className='card-container'>
       <div className="card">
         <img src={article.urlToImage}alt="Image illustrating the article"></img>
         <h3>{article.title}</h3>
         <p>{article.formattedDate} —{article.description}</p>
         <p>| {article.type}</p>
       </div>
-    </div>
   );
 }
 
